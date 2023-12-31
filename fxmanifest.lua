75a02eb6b5ac5bb67223296ea795ac85149b2b4e
s1nyx <sinyx.contact@gmail.com>
2023-12-31 10:34:30 +0100

    init project

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+author 'S1nScripts'
+version '1.0.0'
+repository 'https://github.com/S1nScripts/s1n_lib'
+
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+
+dependencies {
+    '/server:5848',
+    '/onesync',
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}
